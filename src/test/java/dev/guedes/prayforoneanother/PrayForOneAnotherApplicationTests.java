54018052660257a61a81c84504eb22b0022c81b9
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-03 13:08:50 -0300

    :tada: initial commit

---
+++ src/test/java/dev/guedes/prayforoneanother/PrayForOneAnotherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guedes.prayforoneanother;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration test class for {@link PrayForOneAnotherApplication}.
+ *
+ * @author João Guedes
+ */
+class PrayForOneAnotherApplicationTests {
+    @Test
+    void contextLoads() {}
+}
