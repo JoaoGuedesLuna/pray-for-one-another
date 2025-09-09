af2149ecaee10e047eb7f7a1b71b60e4b9840641
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-09 20:26:01 -0300

    :tada: initial commit

---
+++ src/main/java/dev/guedes/prayforoneanother/PrayForOneAnotherApplication.java
@@ -0,0 +1,13 @@
+package dev.guedes.prayforoneanother;
+
+/**
+ * Main entry point for the application.
+ * This class serves as the bootstrap class that delegates to the Application runner.
+ *
+ * @author João Guedes
+ */
+public class PrayForOneAnotherApplication {
+    public static void main(String[] args) {
+        Application.run(args);
+    }
+}
